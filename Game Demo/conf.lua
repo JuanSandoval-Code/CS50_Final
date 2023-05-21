c40b8705ae7733f084ec1ca19f8cd3ae7613ff45
JuanSandoval-Code <113613146+JuanSandoval-Code@users.noreply.github.com>
2023-05-21 14:27:29 -0500

    Delete Game Demo directory

--- Game Demo/conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-    t.title = "Untitled Game Demo"
-    t.version = "11.4"
-    t.console = true
-    t.window.width = 1280
-    t.window.height = 720
-    t.window.vsync = 0
-end
